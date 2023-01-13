2418da19bacffc508006da490f25009b38c6cd99
Sharath Pai <83855796+Sharath1036@users.noreply.github.com>
2023-01-13 10:07:43 +0530

    Add files via upload

---
+++ feedback.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+    Name <?php $_POST["name"]; ?><br>
+    Email <?php $_POST["email"]; ?><br>
+    Rate us on a scale of 5 <?php $_POST["rating"]; ?><br>
+    Additional comments <?php $_POST["comments"]; ?><br>
+    Submit button <?php $_POST["submit"]; ?><br>
+</body>
+</html>  
+    
+    
+
+            
